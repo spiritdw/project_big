8b45ee38a6d1c68ebf26b8dc7f53a7262c59c7bf
spirit <w2000415@126.com>
2021-03-02 18:39:35 +0800

    complete login/reg

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,9 @@
+// 注意每次调用$.get()或者$.post()或者$.ajax()的时候
+// 会先调用ajaxPrefilter这个函数
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function(options) {
+    // console.log(options.url);
+    // 在发起真正的ajax请求之前，统一拼接请求的根路径
+    options.url = 'http://ajax.frontend.itheima.net' + options.url
+    console.log(options.url);
+})
