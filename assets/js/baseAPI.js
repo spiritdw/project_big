25858bc966bbe71865215a0170aa2073909926ff
spirit <w2000415@126.com>
2021-03-03 11:06:40 +0800

    complete index

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,12 @@ $.ajaxPrefilter(function(options) {
     // 在发起真正的ajax请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
     console.log(options.url);
+
+
+    //统一为有权限的接口,设置header请求头
+    if (options.url.indexOf('/my/') !== 1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
